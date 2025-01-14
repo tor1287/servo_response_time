a993f1d0ba7b77263574102840714a12ae961681
Tor kh <thanapolkhth@gmail.com>
2025-01-14 13:50:51 +0700

    time adjust for integer, rate of check, and analog value to integer

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ uart0 = UART(0, 9600)
 uart0.init(baudrate=9600, bits=8, parity=None, stop=1, tx=0, rx=1)
 start_time = time_ns() #start time value
 
-ADC_pin = Pin(26, Pin.IN)
+ADC_pin = Pin(27, Pin.IN)
 pot_pin = ADC(ADC_pin)
 
 def check_time():
     current_time = time_ns() - start_time
-    current_time_ms = current_time/1_000_000 #convert nano to milli
+    current_time_ms = int(current_time/1000000) #convert nano to milli
     time_message = str(current_time_ms)
     return time_message
 
 def check_pot():
-    pot_value = str(pot_pin.read_u16())
+    pot_value = str(int(pot_pin.read_u16()))
     return pot_value
 
 def main():
@@ -29,8 +29,8 @@ def main():
     while True:
         print_time = check_time()
         print_pot = check_pot()
-        uart0.write(print_time + print_pot + "\n")
-        sleep_ms(40)
+        uart0.write(print_time + " / " + print_pot + "\n")
+        sleep_ms(5)
 
 if __name__ == '__main__':
     main()
